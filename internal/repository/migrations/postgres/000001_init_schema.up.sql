6bbd278554a5a5ba75a51825b81b5481bbb02562
Alexander Alexandrov <alex.mirenki@gmail.com>
2026-09-07 14:10:47 +0300

    fix: use BIGINT instead of BIGSERIAL
    Signed-off-by: Alexander Alexandrov <alex.mirenki@gmail.com>

--- internal/repository/migrations/postgres/000001_init_schema.up.sql
+++ internal/repository/migrations/postgres/000001_init_schema.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
-    id       BIGSERIAL PRIMARY KEY,
+    id       BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     login    TEXT    NOT NULL UNIQUE,
     password TEXT    NOT NULL
 );
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TYPE order_status AS ENUM ('NEW', 'PROCESSING', 'INVALID', 'PROCESSED');
 
 CREATE TABLE IF NOT EXISTS orders (
-    id          BIGSERIAL     PRIMARY KEY,
+    id          BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     user_id     BIGINT        NOT NULL REFERENCES users(id),
     number      TEXT          NOT NULL UNIQUE,
     status      order_status  NOT NULL DEFAULT 'NEW',
@@ -19,7 +19,7 @@ CREATE INDEX idx_orders_user_id ON orders(user_id);
 CREATE INDEX idx_orders_status_new_processing ON orders(status) WHERE status IN ('NEW', 'PROCESSING');
 
 CREATE TABLE IF NOT EXISTS withdrawals (
-    id           BIGSERIAL     PRIMARY KEY,
+    id           BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     user_id      BIGINT        NOT NULL REFERENCES users(id),
     order_number TEXT          NOT NULL,
     sum          NUMERIC(20,2) NOT NULL,
